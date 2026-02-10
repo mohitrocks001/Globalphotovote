d4bcae6f7c12ce3a66f04e0d61c7bdc8b7c9f10c
mohitrocks001 <mohitrocks001@gmail.com>
2026-02-09 23:17:54 -0800

    Create firebase.ts

---
+++ Global Photo/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2NpCJYpkMWK6wk47LEKInkR-nAu3lbh8",
+  authDomain: "picvote-1150a.firebaseapp.com",
+  projectId: "picvote-1150a",
+  storageBucket: "picvote-1150a.firebasestorage.app",
+  messagingSenderId: "783966047062",
+  appId: "1:783966047062:web:9ceac5aa3b208f81eb24f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
